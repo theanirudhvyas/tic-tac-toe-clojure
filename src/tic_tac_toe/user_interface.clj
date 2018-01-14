61ceae67014bc801d793da652ae4e3a4e8fdfdb1
Anirudh <anirudh2403@gmail.com>
2018-01-15 01:41:16 +0530

    adds user interface

---
+++ src/tic_tac_toe/user_interface.clj
@@ -0,0 +1,22 @@
+(ns tic-tac-toe.user-interface
+  (:require [tic-tac-toe.state :as state]
+            [tic-tac-toe.domain-logic :refer [winning-state?]]
+            [clojure.string :as str]))
+
+(defn split-by-whitespace [string]
+  (str/split string #"\s+"))
+
+(defn take-user-input []
+  (into [] (map #(Integer/parseInt %) (split-by-whitespace (read-line)))))
+
+(defn process []
+  (loop [input (take-user-input)]
+    (when-not (winning-state?)
+      (let [x-coordinate (input 0)
+            y-coordinate (input 1)]
+        (if (state/update-state-possible? x-coordinate y-coordinate)
+          (do (state/update-state! x-coordinate y-coordinate)
+              (state/display-state))
+          (println "input value invalid, retry!")))
+      (recur (take-user-input))))
+  )
