9ea9f059758bec2377c9589dc39ede6054ab5e1d
Anirudh <anirudh2403@gmail.com>
2018-01-15 01:39:24 +0530

    add entry point to project

---
+++ src/tic_tac_toe/core.clj
@@ -0,0 +1,8 @@
+(ns tic-tac-toe.core
+  (:gen-class)
+  (:require [tic-tac-toe.user-interface :refer [process]]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (process))
---
+++ test/tic_tac_toe/core_test.clj
@@ -0,0 +1,4 @@
+(ns tic-tac-toe.core-test
+  (:require [clojure.test :refer :all]
+            [tic-tac-toe.core :refer :all]))
+
